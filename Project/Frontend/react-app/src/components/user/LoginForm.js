e1ee83e9077288a20d65f7fb66d0328d16ec7db0
TrungKien269 <33851033+TrungKien269@users.noreply.github.com>
2020-04-18 13:54:03 +0700

    UI Update
    Demo login bằng Google và Facebook

--- Project/Frontend/react-app/src/components/user/LoginForm.js
+++ Project/Frontend/react-app/src/components/user/LoginForm.js
@@ -5,69 +5,117 @@ import { withRouter } from "react-router";
 import { setUserSession } from "../../Utils/Commons";
 import { UserContext } from "../../context/userContext";
 import Swal from "sweetalert2";
+import FacebookLogin from "react-facebook-login";
+import GoogleLogin from "react-google-login";
 
-const Login = (props) => {
+const Login = props => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const { token, refreshToken } = useContext(UserContext);
 
-  const handleUserNameChange = (event) => {
+  const handleUserNameChange = event => {
     setUsername(event.target.value);
   };
 
-  const handlePasswordChange = (event) => {
+  const handlePasswordChange = event => {
     setPassword(event.target.value);
   };
 
-  const handleFormSubmit = (event) => {
+  const handleFormSubmit = event => {
     event.preventDefault();
     axios({
       method: "post",
       url: "http://localhost:5000/api/Login/Signin",
       params: {
         username,
-        password,
-      },
+        password
+      }
     })
-      .then((res) => {
+      .then(res => {
         if (res.data.status) {
           Swal.fire({
             title: "Success",
             text: "Sign up  completely",
-            icon: "success",
+            icon: "success"
           }).then(() => {
             setUserSession(res.data.token);
             refreshToken();
             props.history.push("/");
-          })
+          });
         } else {
           Swal.fire({
             title: "Error",
             text: res.data.message,
-            icon: "error",
+            icon: "error"
           });
         }
       })
-      .catch((err) => {
+      .catch(err => {
         Swal.fire({
           title: "Error",
           text: err,
-          icon: "error",
+          icon: "error"
         });
       });
+  };
+
+  const handleFaceBookClicked = () => {
+    console.log("CLICK");
+  };
+
+  const responseFacebook = response => {
+    console.log(response);
+  };
+
+  const responseGoogle = response => {
+    if (response) {
+      axios({
+        method: "post",
+        url: "http://localhost:5000/api/Login/GoogleSignin",
+        params: {
+          email: response.profileObj.email
+        }
+      })
+        .then(res => {
+          if (res.data.status) {
+            Swal.fire({
+              title: "Success",
+              text: "Sign in  completely",
+              icon: "success"
+            }).then(() => {
+              setUserSession(res.data.token);
+              refreshToken();
+              props.history.push("/");
+            });
+          } else {
+            Swal.fire({
+              title: "Error",
+              text: res.data.message,
+              icon: "error"
+            });
+          }
+        })
+        .catch(err => {
+          Swal.fire({
+            title: "Error",
+            text: err,
+            icon: "error"
+          });
+        });
+    }
   };
 
   return (
     <div className="col-md-6">
       <h1>Login</h1>
-      <form method="post" id="loginForm" onSubmit={(e) => handleFormSubmit(e)}>
+      <form method="post" id="loginForm" onSubmit={e => handleFormSubmit(e)}>
         <div className="field-control">
           <label>Username or Email</label>
           <input
             type="text"
             required
             className="col-md-8"
-            onChange={(e) => handleUserNameChange(e)}
+            onChange={e => handleUserNameChange(e)}
           />
         </div>
         <div className="field-control">
@@ -76,7 +124,7 @@ const Login = (props) => {
             type="password"
             required
             className="col-md-8"
-            onChange={(e) => handlePasswordChange(e)}
+            onChange={e => handlePasswordChange(e)}
           />
         </div>
         <div className="col-md-8 pad-0-0 mar-top-md">
@@ -88,6 +136,30 @@ const Login = (props) => {
         </div>
         <Link to="/confirmemail">Forgot your password ?</Link>
       </form>
+
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <FacebookLogin
+              appId="465730364148971"
+              autoLoad={false}
+              fields="name, email, picture"
+              scope="email"
+              onClick={() => handleFaceBookClicked()}
+              callback={e => responseFacebook(e)}
+            />
+          </div>
+          <div className="col-md-12">
+            <GoogleLogin
+              clientId="595791710812-b26iddsr8lsqntdqrs6cpmpllq65obfd.apps.googleusercontent.com"
+              buttonText="Login"
+              onSuccess={e => responseGoogle(e)}
+              onFailure={e => responseGoogle(e)}
+              cookiePolicy={"single_host_origin"}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
